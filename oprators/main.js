c8834c80f8240bcc2a9e1e821d9a48318b2da902
Anirudh-051 <mego48698@gmail.com>
2025-08-03 18:17:37 +0530

    opretors

---
+++ oprators/main.js
@@ -0,0 +1,9 @@
+//question
+let a = 10;
+let b = 20;
+if(a > 5 && b < 30){
+    console.log("false");
+}
+else{
+    console.log("true");
+}
---
+++ oprators/script.js
@@ -0,0 +1,54 @@
+//operators
+//arithmetic operators
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b % a);
+
+//assignment operators
+let c = 10;
+c += 10;
+console.log(c);
+
+//comparison operators
+let d = 10;
+let e = 20;
+console.log(d == e);
+console.log(d != e);
+console.log(d > e);
+console.log(d < e);
+console.log(d >= e);
+console.log(d <= e);
+
+//logical operators
+let f = 10;
+let g = 20;
+console.log(f && g);
+console.log(f || g);
+console.log(!f);
+console.log(!g);
+
+//bitwise operators
+let h = 10;
+let i = 20;
+console.log(h & i);
+console.log(h | i);
+console.log(h ^ i);
+console.log(~h);
+console.log(~i);
+console.log(h << i);
+console.log(h >> i);
+console.log(h >>> i);
+
+//conditional operators
+let j = 10;
+let k = 20;
+console.log(j ? k : j);
+
+//trinary operators
+let l = 10;
+let m = 20;
+l > m ? console.log("l is greater") : console.log("m is greater");
