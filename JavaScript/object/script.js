3342f5443a17e4fd48aefe3d9dcc0fa4a7b76337
Anirudh-051 <mego48698@gmail.com>
2025-08-08 23:41:19 +0530

    ha

---
+++ JavaScript/object/script.js
@@ -0,0 +1,11 @@
+let obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+for(let i in obj){
+    console.log(i);
+}
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.city);
